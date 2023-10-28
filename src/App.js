12fc75241304ca7af428578ca9551e990f62f515
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-10-28 19:30:14 +0600

    Updated API URL to https

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ export default function App() {
           setError('');
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
