4662f2ada86bdaecab24034a283c44e3d0a6818f
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-10-28 14:08:06 +0600

    Added cleaning up function for data fetching

--- src/App.js
+++ src/App.js
@@ -82,13 +82,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError('');
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -101,7 +104,9 @@ export default function App() {
           setMovies(data.Search);
         } catch (e) {
           console.error(e.message);
-          setError(e.message);
+          if (e.name !== 'AbortError') {
+            setError(e.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -113,6 +118,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
