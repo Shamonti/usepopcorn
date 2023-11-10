bddfe1965281d8d58fd02fe3ed2b3ae9b52a1d5d
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-11-10 22:11:12 +0600

    Used useRef to keep search bar in focus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import StarRating from './StarRating';
 
 const average = arr =>
@@ -167,6 +167,34 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === 'Enter') {
+          inputEl.current.focus();
+          setQuery('');
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+    },
+
+    [setQuery]
+  );
+
+  // useEffect(
+  //   function () {
+  //     const el = document.querySelector('.search');
+  //     console.log(el);
+  //     el.focus();
+  //   },
+  //   [query]
+  // );
+
   return (
     <input
       className='search'
@@ -174,6 +202,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={e => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
